46d74c3c36ada3e2d9048362936c70421f63b4d1
Usman-Choudharys <bsf2105191@gmail.com>
2024-02-15 06:56:08 +0500

    write the router in express

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,47 @@
 import { Router } from "express";
-import { registrationUser,loginUser, logedOutUser, refreshAccessToken } from "../controllers/user.Controller.js";
+import {
+	registrationUser,
+	loginUser,
+	logedOutUser,
+	refreshAccessToken,
+	changeCurrentPassowrd,
+	getCurrentUser,
+	updateUserAvatar,
+	updateUserDetails,
+	updateUserCoverImage,
+	getUserChannelProfile,
+	getUserWatchHistory,
+} from "../controllers/user.Controller.js";
 import { upload } from "../middlewares/Multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-     {
-        name:"avatar",
-        maxCount:1
-     },
-     {
-        name:"coverImage",
-        maxCount:1
-     }
-    ]),
-    registrationUser)
+	upload.fields([
+		{
+			name: "avatar",
+			maxCount: 1,
+		},
+		{
+			name: "coverImage",
+			maxCount: 1,
+		},
+	]),
+	registrationUser
+);
 
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT,logedOutUser)
-router.route("/refresh-accessToken").post(refreshAccessToken)
-export default router
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logedOutUser);
+router.route("/refresh-accessToken").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassowrd);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateUserDetails);
+router
+	.route("/avatar")
+	.patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+	.route("/cover-image")
+	.patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
+export default router;
